6c72e06ddfcac2d4df5e1ce0104c8d0d9046c784
gabrielweslley <gabrielnascimento.2@hotmail.com>
2025-09-09 22:10:21 +0000

    adicionando divisao

--- src/calculadora.js
+++ src/calculadora.js
@@ -18,3 +18,18 @@
         add,
         subtract
     };
+
+    // Função de divisão
+    function divide(a, b) {
+        if (b === 0) {
+        throw new Error("Divisão por zero não é permitida.");
+        }
+        return a / b;
+    }
+
+    // Exportar as funções
+    module.exports = {
+      add,
+      subtract,
+      divide,
+    };
