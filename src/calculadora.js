1602795eaf28d0e9b099c2d9810610d29c9dd494
gabrielweslley <gabrielnascimento.2@hotmail.com>
2025-09-09 22:09:16 +0000

    adicionando multiplicacao

--- src/calculadora.js
+++ src/calculadora.js
@@ -18,3 +18,15 @@
         add,
         subtract
     };
+
+    // Função de multiplicação
+    function multiply(a, b) {
+        return a * b;
+    }
+
+    // Exportar as funções
+    module.exports = {
+      add,
+      subtract,
+      multiply
+    };
